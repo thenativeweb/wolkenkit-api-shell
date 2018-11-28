077802d7ede7a235f4c97ec5c923a15130bb4e1b
Matthias Wagler <wagler@intuity.de>
2018-11-28 10:36:33 +0100

    Add TestAndDebug to Information screen.

--- src/screens/Information/Configuration.js
+++ src/screens/Information/Configuration.js
@@ -8,7 +8,7 @@ const styles = theme => ({
   }
 });
 
-const Configuration = ({ classes }) => (
+const Configuration = React.memo(({ classes }) => (
   <div id='screen-information-configuration' className={ classes.Configuration }>
     <Headline id='screen-information-configuration-headline'>Configuration</Headline>
     <Text id='screen-information-configuration-text' breakLines={ true }>
@@ -16,6 +16,6 @@ const Configuration = ({ classes }) => (
       is available at <Link href={ `/v1/configuration.json` }>/v1/configuration.json</Link>.
     </Text>
   </div>
-);
+));
 
 export default injectSheet(styles)(Configuration);
---
+++ src/screens/Information/TestAndDebug.js
@@ -0,0 +1,46 @@
+import injectSheet from 'react-jss';
+import React from 'react';
+import { Button, Headline, Link, Text } from 'thenativeweb-ux';
+
+const styles = theme => ({
+  TestAndDebug: {
+    padding: theme.grid.stepSize * 2
+  }
+});
+
+const getConsoleParameters = function () {
+  const parameters = {
+    host: window.location.hostname,
+    port: window.location.port
+  };
+
+  const parametersAsQueryString = Object.keys(parameters).reduce((queryString, name, index) => {
+    switch (index) {
+      case 0:
+        return `?${name}=${parameters[name]}`;
+      default:
+        return `${queryString}&${name}=${parameters[name]}`;
+    }
+  }, '');
+
+  return parametersAsQueryString;
+};
+
+const getConsoleUrl = function () {
+  const url = `http://localhost:4000/${getConsoleParameters()}`;
+
+  return url;
+};
+
+const TestAndDebug = React.memo(({ classes }) => (
+  <div id='screen-information-test-debug' className={ classes.TestAndDebug }>
+    <Headline id='screen-information-test-debug-headline'>Test & Debug</Headline>
+    <Text id='screen-information-test-debug-text' breakLines={ true }>
+      Connect to and play with this API. Send commands, observe events and read models.
+      using <Link href={ getConsoleUrl() }>console.wolkenkit.io</Link>.
+    </Text>
+    <Link href={ getConsoleUrl() } isExternal={ true }><Button>Connect to this API</Button></Link>
+  </div>
+));
+
+export default injectSheet(styles)(TestAndDebug);
